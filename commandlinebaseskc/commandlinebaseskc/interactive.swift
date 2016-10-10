9509079925010e3a1645816b3ab3680f1116c9a2
Sean Crowl <sean.crowl@interapthq.com>
2016-10-10 11:38:37 -0400

    added base classes for gathering user input

---
+++ commandlinebaseskc/commandlinebaseskc/interactive.swift
@@ -0,0 +1,35 @@
+//
+//  interactive.swift
+//  commandlinebaseskc
+//
+//  Created by Sean Crowl on 10/10/16.
+//  Copyright © 2016 Interapt. All rights reserved.
+//
+
+import Foundation
+
+class Interactive {
+    private var done: Bool = false
+    private var currentInput: String = ""
+    private var io = Io()
+    
+    func go() {
+        while !done {
+            
+            // Ask the user for input right here.
+            io.writeMessage("\nInput?")
+            currentInput = io.getInput()
+            
+            if currentInput == "q" {
+                done = true
+            } else {
+                print("The input is: \(currentInput)")
+            }
+            
+        }
+        print("Exiting....")
+        
+        return
+    }
+    
+}
